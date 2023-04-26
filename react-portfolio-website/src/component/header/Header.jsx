82bb29d2c17b613c91ecc3c46b6c2bb7d0be3c2c
LanaAdekola <adekolaadelana@gmail.com>
2023-04-26 16:54:06 +0100

    new update

--- react-portfolio-website/src/component/header/Header.jsx
+++ react-portfolio-website/src/component/header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/adekola.png'
+
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+      <header>
+        <div className='container header__container'>
+          <h5>Hello I'm</h5>
+          <h1>Adekola Adelana</h1>
+          <h5 className='text-light'>Frontend Developer</h5>
+          <CTA />
+          <HeaderSocials />
+          <div>
+            <img src= {ME} alt='Adekola'/>
+          </div>
+          <a href='#contact' className='scroll__down'>Scroll Down</a>
+        </div>
+      </header>
+    )
 }
 
 export default Header
---
+++ react-portfolio-website/src/component/header/HeaderSocials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://www.linkedin.com/in/adekola-adelana-94393b165/' target='_blank'><BsLinkedin /></a>
+        <a href='https://github.com/LanaAdekola' target='_blank'><BsGithub /></a>
+        <a href='https://www.instagram.com/ade_flashcuts/' target='_blank'><BsInstagram/></a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
