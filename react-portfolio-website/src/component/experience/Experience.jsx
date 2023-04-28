10b7e0fa9536bff09eca62de6ca3df51b1bf5917
LanaAdekola <adekolaadelana@gmail.com>
2023-04-28 17:38:53 +0100

    latest update

--- react-portfolio-website/src/component/experience/Experience.jsx
+++ react-portfolio-website/src/component/experience/Experience.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './experience.css'
-import {} from 'react-icons'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
@@ -13,7 +13,130 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>React js</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>APIs</h4>
+                <small className='text-light'>Professional</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Responsive Web Design</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>UI Design</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Git & Github</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Product Management</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
 
+
+        <div className="experience__frontend">
+          <h3>Video Production</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Adobe Priemere Pro</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Cinematography</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Aesthetics and Creativity</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Social Media Marketing</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Video editing</h4>
+                <small className='text-light'>Professional</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Script Writing</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Screen Writing</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon' /> 
+              <div>
+                <h4>Project Management</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
           </div>
         </div>
